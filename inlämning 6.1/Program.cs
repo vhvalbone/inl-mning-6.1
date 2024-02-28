bfda0e1ba8282bdca1ab80d01d208630d588f488
vhvalbone <albin06svensson@gmail.com>
2024-02-28 10:33:45 +0100

    Add project files.

---
+++ inlämning 6.1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int summa = Addera(5, 10, 15);
+        Console.WriteLine("Summan är: " + summa);
+    }
+
+    static int Addera(int tal1, int tal2, int tal3)
+    {
+        return tal1 + tal2 + tal3;
+    }
+}
